8a34891031109bb9d514d4e6f1d6ec5bea1db31d
dellingc <35514357+dellingc@users.noreply.github.com>
2020-02-20 22:09:23 -0500

    reverted CORS

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://dellinco-wxapp.netlify.com/"); 
+    res.header("Access-Control-Allow-Origin", "*"); 
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
