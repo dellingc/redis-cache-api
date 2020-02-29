014001486011d4bc2bc3a92bc57cd1e845edf9ef
dellingc <35514357+dellingc@users.noreply.github.com>
2020-02-28 22:23:39 -0500

    added retry_strategy

--- server.js
+++ server.js
@@ -11,9 +11,24 @@ app.use(function(req, res, next) {
     res.header("Vary: origin");
     next();
   });
- 
 
-const client = redis.createClient(REDIS_URL)
+//Heroku redis instance port  
+//process.env.REDIS_URL
+const client = redis.createClient(process.env.REDIS_URL{
+    retry_strategy: function(options) {
+        if (options.attempt > 5) {
+            // End reconnecting with built in error
+            return new Error('Max number of attempts reached');
+        }
+        if (options.error && options.error.code === "ECONNREFUSED") {
+          // End reconnecting on a specific error and flush all commands with
+          // a individual error
+          //return new Error("The server refused the connection");
+          console.log(options.error.code + ' - attempt - ' + options.attempt)
+          return new Error('The server refused the connection')
+        } 
+    },
+})
 
 // Log redis errors to the console
 client.on('error', (err) => {
