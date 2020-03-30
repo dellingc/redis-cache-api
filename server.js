449e8fec83bdc5aab5d88131e581aca34ead24c9
dellingc <35514357+dellingc@users.noreply.github.com>
2020-03-30 16:43:37 -0400

    updated cors url

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express()
 const PORT = process.env.PORT || 8000
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://dellincoinc-wxapp.netlify.com/"); 
+    res.header("Access-Control-Allow-Origin", "https://dellincoinc-wxapp.netlify.com"); 
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Vary: origin");
     next();
